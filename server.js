3ba80dfdc7c740b00e545d53d0bf112e7422b70e
Johannal1n <145763921+Johannal1n@users.noreply.github.com>
2024-10-08 18:22:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,20 @@
 
 const http = require('http');
 const fs = require('fs');
-const port = 3000;
+const port = 3000; //portin määritys
+// tuo sisäänrakennetun http moduulin
 
-const requestHandler = (req, res) => {
-    if (req.url.startsWith('/asetukset')) {
-        fs.readFile('settings.txt', 'utf8', (err, data) => {
+
+const requestHandler = (req, res) => { //käsittelee saapuvat http-pyynnöt
+    if (req.url.startsWith('/settings')) {
+        fs.readFile('settings.txt', 'utf8', (err, data) => { //lukee tideoston, katso muuten ne ovatko oikeat lukemat!!
             if (err) {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Error reading settings file');
                 return;
             }
 
-            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.writeHead(200, { 'Content-Type': 'application/json' }); //Muuttaa datan json-objektiksi.
             res.end(JSON.stringify({ settings: data }));
         });
     } else {
@@ -22,10 +24,10 @@ const requestHandler = (req, res) => {
     }
 };
 
-const server = http.createServer(requestHandler);
+const server = http.createServer(requestHandler); //luo http palvelimen käyttäen http-moduulia
 
 server.listen(port, () => {
-    console.log(Server is running on http://localhost:${port});
+    console.log(Server is running on http://localhost:${port}); //kuuntelee tuolla aiemmin määriteltyä porttia 3000
 });
 
 
